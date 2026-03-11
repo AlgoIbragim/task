0fb294060b628f67c2226fb929bb81273cec1097
AlgoIbragim <algoritmika543@gmail.com>
2026-03-11 22:59:00 +0500

    Добавлена проверка деления на ноль. Fixes #16

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 def add(first_number, second_number):
-    """Складывает два числа и возвращает результат."""
     return first_number + second_number
 
 def sub(first_number, second_number):
-    """Вычитает второе число из первого и возвращает результат."""
     return first_number - second_number
 
 def mul(first_number, second_number):
-    """Умножает два числа и возвращает результат."""
     return first_number * second_number
 
 def div(first_number, second_number):
-    """Делит первое число на второе и возвращает результат."""
     return first_number / second_number
 
 if __name__ == "__main__":
-    # Запрашиваем данные у пользователя (преобразуем введенную строку в целое число)
-    first_number = int(input('Введите первое число: '))
-    second_number = int(input('Введите второе число: '))
+    first_input = input('Введите первое число: ')
+    second_input = input('Введите второе число: ')
     
-    # Выводим результаты с понятными текстовыми пояснениями
+    # Преобразуем в числа
+    first_number = int(first_input)
+    second_number = int(second_input)
+    
     print(f"Результат сложения ({first_number} + {second_number}) = {add(first_number, second_number)}")
     print(f"Результат вычитания ({first_number} - {second_number}) = {sub(first_number, second_number)}")
     print(f"Результат умножения ({first_number} * {second_number}) = {mul(first_number, second_number)}")
-    print(f"Результат деления ({first_number} / {second_number}) = {div(first_number, second_number)}")
+    
+    # Добавили проверку на 0, НО ЗАБЫЛИ ПРОВЕРКУ НА БУКВЫ!
+    if second_number == 0:
+        print("Ошибка: на 0 делить нельзя!")
+    else:
+        print(f"Результат деления ({first_number} / {second_number}) = {div(first_number, second_number)}")
