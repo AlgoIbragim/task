a5de8303e55cd53909aaea9e1d391eef93703b43
AlgoIbragim <algoritmika543@gmail.com>
2026-03-11 22:42:28 +0500

    Добавлена функция деления и обновлены тесты. Fixes #15

--- app.py
+++ app.py
@@ -10,12 +10,17 @@ def mul(first_number, second_number):
     """Умножает два числа и возвращает результат."""
     return first_number * second_number
 
+def div(first_number, second_number):
+    """Делит первое число на второе и возвращает результат."""
+    return first_number / second_number
+
 if __name__ == "__main__":
     # Запрашиваем данные у пользователя (преобразуем введенную строку в целое число)
     first_number = int(input('Введите первое число: '))
     second_number = int(input('Введите второе число: '))
     
     # Выводим результаты с понятными текстовыми пояснениями
-    print(f"Результат сложения ({first_number} + {second_number}): {add(first_number, second_number)}")
-    print(f"Результат вычитания ({first_number} - {second_number}): {sub(first_number, second_number)}")
-    print(f"Результат умножения ({first_number} * {second_number}): {mul(first_number, second_number)}")
+    print(f"Результат сложения ({first_number} + {second_number}) = {add(first_number, second_number)}")
+    print(f"Результат вычитания ({first_number} - {second_number}) = {sub(first_number, second_number)}")
+    print(f"Результат умножения ({first_number} * {second_number}) = {mul(first_number, second_number)}")
+    print(f"Результат деления ({first_number} / {second_number}) = {div(first_number, second_number)}")
